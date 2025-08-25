41288c2ced928ad612d30cb0c93876401cf1626c
adrian-mardia-py <adrian.mardia@gmail.com>
2025-08-26 00:40:13 +0200

    Separate the main and the class in different files

--- LinealRegresion.py
+++ LinealRegresion.py
@@ -9,7 +9,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from numba import cuda
-from time import time
 
 import warnings
 from numba.core.errors import NumbaPerformanceWarning
@@ -389,24 +388,3 @@ def actualizarParametros(
     #Actualizamos los parámetros
     if idx < n_parametros:
         cuda.atomic.sub(parametros_cuda, (idx, 0), alpha[0] * gradiente_cuda[idx, 0] / n_puntos)
-
-def main() -> None:
-    """
-    Pequeña muestra simple de las capacidades del modelo de regresión lineal
-    """
-    dispersion = float(input("Introduzca como de difuminado quiere la muestra-> "))
-    m = int(input("Introduzca la pendiente -> "))
-    n = int(input("Iltroduzca la ordenada en el origen -> "))
-    X = np.linspace(0, 10, 100).reshape(-1, 1)
-    y = m * X + n + np.random.randn(100, 1) * dispersion
-
-    iteraciones = int(input("\nIntroduzca el numero de iteraciones que desea realizar. \nDeben ser más de 300 iteraciones, si no se fijarán automáticamente\n\n-------> "))
-    gpu = bool(input("\n¿Quiere hacer los cálculos en GPU? (Si/No) --> ").upper() == 'SI')
-
-    modelo = regresionLineal(target = 'gpu' if gpu else 'cpu', n_iter = iteraciones, printECM=True)
-
-    modelo.fit_transform(X, y, plot=True)
-
-
-if __name__ == "__main__":
-    main()
